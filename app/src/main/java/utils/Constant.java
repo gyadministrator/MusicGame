804d93ec0cc58111f72462d9a8194123d737f5fe
gy <1984629668@qq.com>
2017-12-29 16:15:38 +0800

    更改了服务器地址。

--- app/src/main/java/utils/Constant.java
+++ app/src/main/java/utils/Constant.java
@@ -5,5 +5,5 @@ public class Constant {
     /**
      * 服务器基地址
      */
-    public static final String BASE_URL = "http://112.74.43.236/MusicWebServer";
+    public static final String BASE_URL = "http://123.207.93.53/MusicWebServer";
 }

f3394774f316ebb27fab0d5bdeda26e8064fe835
gy <1984629668@qq.com>
2017-12-29 22:16:28 +0800

    实现了手机号注册。

---
+++ app/src/main/java/utils/MobUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by Administrator on 2017/12/29.
+ */
+
+public class MobUtils {
+    public static final String APP_KEY = "2364a959ab864";
+    public static final String APP_SECRET = "5d7b69640bcaaccb059faa8d70ada951";
+}

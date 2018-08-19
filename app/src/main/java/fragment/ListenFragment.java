4a225019c423773e67670ba271336b4bb52bcaac
高运 <1562211757@qq.com>
2018-08-19 14:46:43 +0800

    修复一些bug

--- app/src/main/java/fragment/ListenFragment.java
+++ app/src/main/java/fragment/ListenFragment.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
